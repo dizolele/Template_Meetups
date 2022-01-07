e1d19493b1244d907f7f640b5fdbce3c2366431c
Dizolele Daniel Pedro <dizolele.pedro@iteris.com.br>
2022-01-07 16:31:31 -0300

    template commit

---
+++ Meetups.API/Meetups.API/Program.cs
@@ -0,0 +1,34 @@
+using Meetups.API.DAL;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Retorna Connection String da lista de configurações
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<MeetupsContext>(options => options.UseSqlServer(connectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ README.md
@@ -0,0 +1,6 @@
+# Templates
+
+Este projeto contém 2 templates de projetos:
+
+- [Meetups.API](./Meetups.API/README.md): API ASP.NET 6.0 com acesso e entendidades do banco de dados.
+- [meetups-web](./meetups-web): Front Vue 2 com Rotas, Vuetify e Cliente HTTP.
